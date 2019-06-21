25bf78f69397238a8bf5b3647b2f7df8d70e0218
Anika <info@anikaszuppa.de>
2019-06-21 08:48:45 +0000

    cleanup node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,11 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: ['babel-loader', 'eslint-loader']
+                use: ['babel-loader', 'eslint-loader'],
             },
-            {
-                test: /\.(scss|css)$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
+        ],
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve('./index.html')
-        })
-    ]
 };
